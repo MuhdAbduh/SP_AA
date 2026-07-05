3690740f170d1b215fff0f6d8839ceb7aa13ac09
Muhammad Abduh Bin Abdul Ba'ari <abduhmuhammad979@gmail.com>
2026-07-06 02:51:49 +0800

    ok

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MedVault System</title>
+</head>
+<body>
+
+<h1>MedVault Secure System</h1>
+
+<ul>
+    <li><a href="search_form.php">Search Patient Records</a></li>
+    <li><a href="auth_form.php">Authentication Test</a></li>
+</ul>
+
+</body>
+</html>
